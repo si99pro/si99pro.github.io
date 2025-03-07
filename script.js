e5fc97c80e7bc90c42815d177a194b01664cffe6
Shahidul Islam <170623757+si99pro@users.noreply.github.com>
2025-03-07 23:41:14 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+
+// You can add some basic interactivity here, like smooth scrolling
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
